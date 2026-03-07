dc2255de84e339f27e067bbc5e30b1ef3169a416
krsnaone <krsnaone@gmail.com>
2026-03-08 03:16:57 +0800

    Delete .github/workflows directory

--- .github/workflows/app/src/main/java/com/puzzlebot/NumberResult.kt
+++
@@ -1,7 +0,0 @@
-package com.puzzlebot
-
-data class NumberResult(
-    val number: Int,
-    val screenX: Float,
-    val screenY: Float
-)
--- .github/workflows/gradlew.bat
+++
@@ -1,2 +0,0 @@
-@rem Gradle startup script for Windows
-@rem This file is not needed for GitHub Actions (Linux) but included for completeness
